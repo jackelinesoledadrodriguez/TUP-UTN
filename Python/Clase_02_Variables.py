a4974d5b08ba3d191e560b299e1e9ffcc08ce51a
jackelinesoledadrodriguez <jackelinesoledadrodriguez@gmail.com>
2025-05-27 16:46:02 -0300

    Carpeta Python

---
+++ Python/Clase_02_Variables.py
@@ -0,0 +1,14 @@
+miVariable = 3
+print(miVariable)
+miVariable ="Hola a todos los estudiantes de la tecnicatura"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+
+x=10
+y=2
+z=x+y
+print(z)
+print(id(x))  #En que casilla de memoria está almacenada x
+print(id(y))
+print(id(z))
